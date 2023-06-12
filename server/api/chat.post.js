da0ef8fe34f5bc5c606b5cb811b286c60ee42a17
MJMOSH <42127006+Johnnnmai@users.noreply.github.com>
2023-06-12 12:08:59 -0700

    Update chat.post.js

--- server/api/chat.post.js
+++ server/api/chat.post.js
@@ -1,38 +1,29 @@
 export default defineEventHandler(async (event) => {
-    const config = useRuntimeConfig();
-    let messages = [];
-    const previousMessages = await readBody(event);
+	const config = useRuntimeConfig();
+	let messages = [];
+	const previousMessages = await readBody(event);
+	messages = messages.concat(previousMessages);
+	let prompt = messages.map((message) => {
+		return {
+			role: message.role === 'AI' ? 'assistant' : 'user',
+			content: message.message
+		};
+	});
+	const req = await fetch('https://api.openai.com/v1/chat/completions', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${config.OPENAI_API_KEY}`
+		},
+		body: JSON.stringify({
+			model: 'gpt-3.5-turbo',
+			messages: prompt
+		})
+	});
 
-    // Add system message
-    messages.push({
-        role: "system",
-        content: "You are a helpful assistant."
-    });
-
-    // Add previous user and assistant messages
-    for (let message of previousMessages) {
-        messages.push({
-            role: message.role,
-            content: message.message
-        });
-    }
-
-    const req = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${config.OPENAI_API_KEY}`
-        },
-        body: JSON.stringify({
-            model: 'gpt-3.5-turbo',
-            messages: messages
-        })
-    });
-
-    const res = await req.json();
-    const result = res.choices[0].message;
-
-    return {
-        message: result.content
-    };
+	const res = await req.json();
+	const result = res.choices[0].message.content;
+	return {
+		message: result
+	};
 });
