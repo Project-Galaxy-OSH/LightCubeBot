ce7614da1ce3352bdb7e88587375704ddb10f76c
MJMOSH <42127006+Johnnnmai@users.noreply.github.com>
2023-06-12 11:43:57 -0700

    Update chat.post.js
    Updating GPT3.5 MODEL

--- server/api/chat.post.js
+++ server/api/chat.post.js
@@ -1,8 +1,8 @@
 export default defineEventHandler(async (event) => {
 	const config = useRuntimeConfig();
 	let messages = [];
-	const previosMessages = await readBody(event);
-	messages = messages.concat(previosMessages);
+	const previousMessages = await readBody(event);
+	messages = messages.concat(previousMessages);
 	let prompt =
 		messages.map((message) => `${message.role}: ${message.message}`).join('\n') + `\nAI:`;
 	const req = await fetch('https://api.openai.com/v1/completions', {
@@ -12,20 +12,23 @@ export default defineEventHandler(async (event) => {
 			Authorization: `Bearer ${config.OPENAI_API_KEY}`
 		},
 		body: JSON.stringify({
-			model: 'text-davinci-003',
-			prompt: prompt,
-			temperature: 0.9,
-			max_tokens: 512,
-			top_p: 1.0,
-			frequency_penalty: 0,
-			presence_penalty: 0.6,
-			stop: [' User:', ' AI:']
+			model: 'gpt-3.5-turbo',
+			messages: [
+				{
+					role: 'system',
+					content: 'You are a helpful assistant.'
+				},
+				{
+					role: 'user',
+					content: prompt
+				}
+			]
 		})
 	});
-
 	const res = await req.json();
 	const result = res.choices[0];
 	return {
-		message: result.text
+		message: result.message.content
 	};
 });
+
