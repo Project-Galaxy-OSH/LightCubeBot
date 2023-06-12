d78ea3824d8e072072298f864cc5473e9d5e1f81
MJMOSH <42127006+Johnnnmai@users.noreply.github.com>
2023-06-12 13:14:27 -0700

    Update chat.post.js

--- server/api/chat.post.js
+++ server/api/chat.post.js
@@ -1,39 +1,31 @@
 export default defineEventHandler(async (event) => {
-	const config = useRuntimeConfig();
-	let messages = [];
-	const previousMessages = await readBody(event);
-	messages = messages.concat(previousMessages);
+  const config = useRuntimeConfig();
+  let messages = [];
+  const previosMessages = await readBody(event);
+  messages = messages.concat(previosMessages);
+  let prompt =
+    messages.map((message) => `${message.role}: ${message.message}`).join('\n') + `\nAI:`;
+  const req = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${config.OPENAI_API_KEY}`
+    },
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        ...messages.map((message) => ({
+          role: message.role === 'AI' ? 'assistant' : 'user',
+          content: message.message
+        }))
+      ]
+    })
+  });
 
-	// Convert the messages to the new format
-	const convertedMessages = messages.map((message) => {
-		return {
-			role: message.role.toLowerCase(),
-			content: message.message
-		};
-	});
-
-	// Add a system message at the start if it doesn't exist
-	if (convertedMessages.length === 0 || convertedMessages[0].role !== 'system') {
-		convertedMessages.unshift({
-			role: 'system',
-			content: 'You are a helpful assistant.'
-		});
-	}
-
-	const req = await fetch('https://api.openai.com/v1/engines/davinci-codex/answers', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${config.OPENAI_API_KEY}`
-		},
-		body: JSON.stringify({
-			model: 'gpt-3.5-turbo',
-			messages: convertedMessages
-		})
-	});
-
-	const res = await req.json();
-	return {
-		message: res.choices[0].message.content
-	};
+  const res = await req.json();
+  const result = res.choices[0].message;
+  return {
+    message: result.content
+  };
 });
