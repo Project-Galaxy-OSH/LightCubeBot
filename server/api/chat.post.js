ddc0d392ce72b895f3309698c9ce90b7f6a3c6e4
MJMOSH <42127006+Johnnnmai@users.noreply.github.com>
2023-06-12 11:56:32 -0700

    Update chat.post.js

--- server/api/chat.post.js
+++ server/api/chat.post.js
@@ -1,34 +1,38 @@
 export default defineEventHandler(async (event) => {
-	const config = useRuntimeConfig();
-	let messages = [];
-	const previousMessages = await readBody(event);
-	messages = messages.concat(previousMessages);
-	let prompt =
-		messages.map((message) => `${message.role}: ${message.message}`).join('\n') + `\nAI:`;
-	const req = await fetch('https://api.openai.com/v1/completions', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${config.OPENAI_API_KEY}`
-		},
-		body: JSON.stringify({
-			model: 'gpt-3.5-turbo',
-			messages: [
-				{
-					role: 'system',
-					content: 'You are a helpful assistant.'
-				},
-				{
-					role: 'user',
-					content: prompt
-				}
-			]
-		})
-	});
-	const res = await req.json();
-	const result = res.choices[0];
-	return {
-		message: result.message.content
-	};
-});
+    const config = useRuntimeConfig();
+    let messages = [];
+    const previousMessages = await readBody(event);
 
+    // Add system message
+    messages.push({
+        role: "system",
+        content: "You are a helpful assistant."
+    });
+
+    // Add previous user and assistant messages
+    for (let message of previousMessages) {
+        messages.push({
+            role: message.role,
+            content: message.message
+        });
+    }
+
+    const req = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${config.OPENAI_API_KEY}`
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: messages
+        })
+    });
+
+    const res = await req.json();
+    const result = res.choices[0].message;
+
+    return {
+        message: result.content
+    };
+});
